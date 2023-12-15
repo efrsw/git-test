cae9396d0feeb31e6fef5b2ba65c718b11430ac2
Egor Fursov <egorf@live.ru>
2023-12-15 14:14:45 +0300

    Changes in master

--- consoleCringe.js
+++ consoleCringe.js
@@ -1,3 +1,6 @@
 console.log("I want damn conflicts");
 console.log("The development continues!");
 console.log("Here will be THE CONFLICTS!");
+
+// These are changes in master
+console.log("Changes in master");
