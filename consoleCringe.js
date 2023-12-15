c43881dab5f8cba581bd37bf051137b9d01ab5d3
Egor Fursov <egorf@live.ru>
2023-12-15 14:32:25 +0300

    Two branches tracking one remote, merged

--- consoleCringe.js
+++ consoleCringe.js
@@ -3,4 +3,6 @@ console.log("The development continues!");
 console.log("Here will be THE CONFLICTS!");
 
 // These are changes in master
+// These are changes from Feature1 branch
 console.log("Changes in master");
+console.log("Diff branch");
