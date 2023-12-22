f8ea0fd1ac957cd146a40a9bdf3237a16abf76e5
Egor Fursov <fursov.egor@gmail.com>
2023-12-22 14:53:22 +0300

    Some commit to test

--- consoleCringe.js
+++ consoleCringe.js
@@ -2,7 +2,7 @@ console.log("I want damn conflicts");
 console.log("The development continues!");
 console.log("Here will be THE CONFLICTS!");
 
-// These are changes in master
-// These are changes from Feature1 branch
 console.log("Changes in master");
 console.log("Diff branch");
+
+console.log("These are new changes");
